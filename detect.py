8cec9143beaab292c8a5ec0380cde03bd1b44d3f
Neptune <bao2703@gmail.com>
2017-10-17 16:31:15 +0700

    Up

--- detect.py
+++ detect.py
@@ -3,19 +3,28 @@ import cv2
 import os
 from moviepy.editor import VideoFileClip
 
-stop_casade = cv2.CascadeClassifier("cascade/stop_cascade.xml")
-left_casade = cv2.CascadeClassifier("cascade/left_cascade.xml")
-right_casade = cv2.CascadeClassifier("cascade/right_cascade.xml")
+s = ''
+stop_cascade = cv2.CascadeClassifier("cascade/stop_cascade.xml")
+left_cascade = cv2.CascadeClassifier("cascade/left_cascade.xml")
+right_cascade = cv2.CascadeClassifier("cascade/right_cascade.xml")
 
-def detect(img):
+def detect(img, i):
+    img = cv2.resize(img, (250, 450))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    stop = stop_casade.detectMultiScale(gray, 1.02, 10)
-    left = left_casade.detectMultiScale(gray, 1.02, 10)
-    right = right_casade.detectMultiScale(gray)
+    stop = stop_cascade.detectMultiScale(gray, 1.02, 10)
+    left = left_cascade.detectMultiScale(gray, 1.02, 10)
+    right = right_cascade.detectMultiScale(gray, 1.02, 10)
+    global s
     for (x, y, w, h) in stop:
-        if w > 100:
-            cv2.putText(img, 'Stop', (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), 2, cv2.LINE_AA)
-            #cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        #cv2.putText(img, 'Stop', (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 0), 2, cv2.LINE_AA)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        s += str(i) + ' 3 \n'
+    for (x, y, w, h) in left:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        s += str(i) + ' 1 \n'
+    for (x, y, w, h) in right:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        s += str(i) + ' 2 \n'
     return img
 
 def load_images_from_folder(folder):
@@ -28,7 +37,7 @@ def load_images_from_folder(folder):
 
 def rename_files(folder):
     images = load_images_from_folder(folder)
-    i = 1318
+    i = 0
     for image in images :
         i += 1
         print('processing')
@@ -36,28 +45,23 @@ def rename_files(folder):
     pass
 
 def detect_folder(folder):
-    images = load_images_from_folder(folder)
-    i = 0
-    for image in images:
-        i += 1
-        try:
-            print('processing')
-            image = detect(image)
-            cv2.imwrite(os.path.join('output', '{0}.jpg'.format(i)), image)
-        except:
-            pass
+    # images = load_images_from_folder(folder)
+    # i = 0
+    # for image in images:
+    #     i += 1
+    #     img = detect(image)
+    #     cv2.imwrite(os.path.join('output', '{0}.jpg'.format(i)), img)
+
+    for i in range(1, 21):
+        img = cv2.imread(os.path.join(folder, '{0}.jpg'.format(i)))
+        img = detect(img, i)
+        cv2.imwrite(os.path.join('output', '{0}.jpg'.format(i)), img)
     pass
 
 def detect_video(input, output):
     video = VideoFileClip(input)
     output = video.fl_image(detect)
     output.write_videofile(output, audio=False)
-    pass
-
-def detect_image(input, output):
-    image = cv2.imread(input)
-    image = detect(image)
-    cv2.imwrite(output, image)
     pass
 
 def write(str):
@@ -67,4 +71,25 @@ def write(str):
     pass
 
 #detect_video('videos/input.avi', 'output.mp4')
-detect_image('test/stop.jpg', 'output.jpg')
+#detect_folder('test2')
+
+
+def real_time():
+    cap = cv2.VideoCapture('videos/7.avi')
+    i = 0
+    while True:
+        success, frame = cap.read()        
+        if success:
+            i += 1
+            img = detect(frame, i)
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
+            print('fail')
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+real_time()
+print(s)

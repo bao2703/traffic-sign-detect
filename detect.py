08ff1d75edaf4eb23ff4b4c032f06fed129b8f8c
Neptune <bao2703@gmail.com>
2017-11-10 23:24:49 +0700

    Up

--- detect.py
+++ detect.py
@@ -2,37 +2,34 @@ import numpy as np
 import cv2
 import os
 
-s = ''
 stop_cascade = cv2.CascadeClassifier("cascade/stop_cascade.xml")
 left_cascade = cv2.CascadeClassifier("cascade/left_cascade.xml")
 right_cascade = cv2.CascadeClassifier("cascade/right_cascade.xml")
 
+KERNEL_SIZE = 3
+def gaussian_blur(img):
+	return cv2.GaussianBlur(img, (KERNEL_SIZE, KERNEL_SIZE), 0)
+
+def grayscale(img):
+	return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
 def detect(img, i):
-    #img = cv2.resize(img, (1000, 600))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    stop = stop_cascade.detectMultiScale(gray, 1.02, 10)
-    left = left_cascade.detectMultiScale(gray, 1.02, 10)
-    right = right_cascade.detectMultiScale(gray, 1.02, 10)
-    global s
+    img = cv2.resize(img, (500, 300))
+    gray = grayscale(img)
+    gaussian = gaussian_blur(gray)
+    stop = stop_cascade.detectMultiScale(gaussian)
+    #left = left_cascade.detectMultiScale(gray, 1.02, 10)
+    #right = right_cascade.detectMultiScale(gray, 1.02, 10)
+        
     for (x, y, w, h) in stop:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-        s += str(i) + ' 3\n'
-        print(3)
-    for (x, y, w, h) in left:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        s += str(i) + ' 1\n'
-        print(1)
-    for (x, y, w, h) in right:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        s += str(i) + ' 2\n'
-        print(2)
+        print(w)
+        print(h)
+    #for (x, y, w, h) in left:
+    #    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    #for (x, y, w, h) in right:
+    #    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     return img
-
-def write(str):
-    file = open('output.txt', 'w')
-    file.write(str)
-    file.close()
-    pass
 
 def real_time():
     cap = cv2.VideoCapture('videos/ROBOGAME.mp4')
@@ -41,13 +38,11 @@ def real_time():
         success, frame = cap.read()        
         if success:
             i += 1
-            print(i)
-            if i == 686 or i == 687 or i == 688 or i == 812 or i == 814 or i == 880 or i == 882 or i == 890 or i == 1050 or i == 1086:
+            if i > 1000:
                 img = detect(frame, i)
-                cv2.imwrite("{0}.jpg".format(i), img)
-                #cv2.imshow('img', img)
-                #if cv2.waitKey(1) & 0xFF == ord('q'):
-                    #break
+                cv2.imshow('img', img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
         else:
             print('fail')
             break
@@ -55,4 +50,3 @@ def real_time():
     cv2.destroyAllWindows()
 
 real_time()
-write(s)
